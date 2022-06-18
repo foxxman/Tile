9a11c6ee912e9c045210ebf8db927cb46824e468
foxxman <chuprov9977@gmail.com>
2022-06-18 13:12:25 +0300

    add favourites

--- src/js/app.js
+++ src/js/app.js
@@ -12,12 +12,26 @@ import * as header from "./modules/header.js";
 
 flsFunctions.isWebp();
 // LAZY LOAD====================
-// document.querySelectorAll("img").forEach((img) => img.classList.add("lazy"));
-
 var lazyLoadInstance = new LazyLoad({
   selector: "img", // all images
 });
-
+// SVG========================
+$("img.img-svg").each(function () {
+  var $img = $(this);
+  var imgClass = $img.attr("class");
+  var imgURL = $img.attr("src");
+  $.get(imgURL, function (data) {
+      var $svg = $(data).find("svg");
+      if (typeof imgClass !== "undefined") {
+          $svg = $svg.attr("class", imgClass + " replaced-svg");
+      }
+      $svg = $svg.removeAttr("xmlns:a");
+      if (!$svg.attr("viewBox") && $svg.attr("height") && $svg.attr("width")) {
+          $svg.attr("viewBox", "0 0 " + $svg.attr("height") + " " + $svg.attr("width"))
+      }
+      $img.replaceWith($svg);
+  }, "xml");
+});
 // HEADER MENU===============
 header.initHeader();
 
