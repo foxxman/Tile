a479d4cd93ae72750c8c7b8965a0fec4fca64b38
foxxman <chuprov9977@gmail.com>
2022-06-24 13:22:58 +0300

    correct about slider

--- src/js/modules/about-shop-slider.js
+++ src/js/modules/about-shop-slider.js
@@ -3,24 +3,24 @@ export const initAboutShopSlider = () => {
   const sliderThumbs = new Swiper(".slider__thumbs .swiper-container", {
     // ищем слайдер превью по селектору
     // задаем параметры
-    direction: "vertical", // вертикальная прокрутка
+    loop: false,
+    watchOverflow: true,
+    autoHeight: false,
     slidesPerView: 3, // показывать по 3 превью
-    spaceBetween: 10, // расстояние между слайдами
-    navigation: {
-      // задаем кнопки навигации
-      nextEl: ".slider__next", // кнопка Next
-      prevEl: ".slider__prev", // кнопка Prev
+    grid: {
+      rows: 4,
+      fill: "row",
     },
-    // freeMode: true, // при перетаскивании превью ведет себя как при скролле
+    spaceBetween: 38, // расстояние между слайдами
     breakpoints: {
       // условия для разных размеров окна браузера
       0: {
         // при 0px и выше
-        direction: "horizontal", // горизонтальная прокрутка
+        // direction: "horizontal", // горизонтальная прокрутка
       },
       768: {
         // при 768px и выше
-        direction: "vertical", // вертикальная прокрутка
+        // direction: "vertical", // вертикальная прокрутка
       },
     },
   });
@@ -28,7 +28,6 @@ export const initAboutShopSlider = () => {
   const sliderImages = new Swiper(".slider__images .swiper-container", {
     // ищем слайдер превью по селектору
     // задаем параметры
-    direction: "vertical", // вертикальная прокрутка
     slidesPerView: 1, // показывать по 1 изображению
     spaceBetween: 32, // расстояние между слайдами
     mousewheel: true, // можно прокручивать изображения колёсиком мыши
@@ -50,7 +49,7 @@ export const initAboutShopSlider = () => {
       },
       768: {
         // при 768px и выше
-        direction: "vertical", // вертикальная прокрутка
+        // direction: "vertical", // вертикальная прокрутка
       },
     },
   });
